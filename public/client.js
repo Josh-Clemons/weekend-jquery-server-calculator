f2cb8b49fc7bb8cb549677118934b6f41184907b
Josh Clemons <mrjoshc@gmail.com>
2022-11-04 16:16:06 -0500

    base goals finished

--- public/client.js
+++ public/client.js
@@ -7,8 +7,11 @@ let inputNumberTwo = 0;
 
 
 function onReady() {
-    console.log('we have jquery');
+    // console.log('we have jquery');
     $('.operation-btn').click(setOperator);
+    $('#equals-btn').click(sendCalculationData);
+    $('#clear-btn').click(clearCalculationData);
+    getCalculationData();
 }
 
 
@@ -18,22 +21,66 @@ function onReady() {
 // send to server as object
 // receive calculation and history from server
 // render to DOM
+// clear history from 
 
 
 function setOperator () {
     // console.log('in setOperator', $(this).attr('id') );
-    operator = $(this).attr('id');
+    operator = $(this).text();
 };
 
 function sendCalculationData () { // build and send object to server
+    let calculationData = {
+        numberOne: $('#first-number-input').val(),
+        numberTwo: $('#second-number-input').val(),
+        operator: operator
+    }
     $.ajax({
         method: 'POST',
         url: '/calculations',
-        data: {
-            numberOne: $('#firstNumberInput').val(),
-            numberTwo: $('#secondNumberInput').val(),
-            operator: operator
-        }
-    }).then // stopping here, need to continue then function
-
+        data: calculationData
+    }).then(function(response){
+        $('#first-number-input').val('');
+        $('#second-number-input').val('');
+        getCalculationData ();
+    }).catch(function(error){
+        alert('failed post', error)
+    });
 };
+
+function getCalculationData () {
+    $.ajax({
+        method: 'GET',
+        url: '/calculations'
+    }).then(function (response) {
+        // console.log(response);
+        render(response);
+    }).catch(function(error) {
+        alert('error running getcalculationdata', error);
+    });
+}
+
+function clearCalculationData () {
+    // console.log('in clearCalculationData');
+    $.ajax({
+        method: 'DELETE',
+        url: '/calculations'
+    }).then(function(response) {
+        getCalculationData();
+    }).catch(function(error){
+        alert('failed clearcalculationdatta', error);
+    });
+}
+
+function render (calc) {
+    // console.log('calc.calculation in render', calc.calculation);
+    $('#calculated-value').empty();
+    $('#calculated-value').append(`Total: ${calc.calculation}`);
+
+    $('#calculation-history-list').empty();
+    for (i=calc.history.length-1; i>=0; i--) {
+        $('#calculation-history-list').append(`
+            <li>${calc.history[i]}</li>
+        `);
+    }
+}
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 const PORT = 5000;
 
+let calculationHistory = [];
+let calculation = 0;
 
 // TASKS FOR SERVER SIDE
 // receive calculator data
@@ -13,3 +15,48 @@ const PORT = 5000;
 // send calculation and history to client
 
 
+app.post ('/calculations' , (req, res) => {
+    calculation = calculateValue(req.body);
+    let equation = `${req.body.numberOne} ${req.body.operator} ${req.body.numberTwo} = ${calculation}`;
+    calculationHistory.push(equation);
+    // console.log('calculation value' , calculation);
+    // console.log('calc history ', calculationHistory);
+    res.sendStatus(200);
+});
+
+app.get('/calculations' , (req, res) =>{
+    res.send({calculation: calculation, history: calculationHistory})
+});
+
+app.delete('/calculations', (req, res) => {
+    calculation = 0;
+    calculationHistory = [];
+    res.sendStatus(200);
+})
+
+
+
+function calculateValue (calc) {
+    let answer = 0;
+    console.log('operation being passed' , calc.operator);
+    switch (calc.operator) {
+        case ('-') : 
+            answer = Number(calc.numberOne) - Number(calc.numberTwo);
+            break;
+        case ('+') :
+            console.log('in plus button switch');
+            answer = Number(calc.numberOne) + Number(calc.numberTwo);
+            break;
+        case ('/') :
+            answer = Number(calc.numberOne) / Number(calc.numberTwo);
+            break;
+        case ('*') :
+            answer = Number(calc.numberOne) * Number(calc.numberTwo);
+            break;
+    }
+    return (answer);
+}
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
