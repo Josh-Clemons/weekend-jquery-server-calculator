6392501fe97a3fd93ead12ed83cd08d5076a01a4
Josh Clemons <mrjoshc@gmail.com>
2022-11-07 11:55:42 -0600

    updated PORT process

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 let calculationHistory = [];
 let calculation = 0;
